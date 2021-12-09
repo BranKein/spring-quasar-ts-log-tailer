697c14d6ed350cfdc6e926176b4dbf4a5c5a91fc
YeonhyukKim <brankein13@gm.gist.ac.kr>
2021-12-09 23:23:38 +0900

    Initial commit

---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title class="text-h6 text-weight-bold">
+          <q-avatar>
+            <q-icon name="fas fa-align-left" />
+          </q-avatar>
+          LogTailer
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs align="left">
+        <q-route-tab to="/tail" label="Log" />
+        <q-route-tab to="/setting" label="Setting" />
+      </q-tabs>
+    </q-header>
+
+    <q-drawer show-if-above side="left" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
---
+++ src/test/java/com/yhkim/logtailer/LogtailerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhkim.logtailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogtailerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
